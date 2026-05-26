d77de9d48482fca3db7754cbadd3085951273db0
Sharon-Mashai <mashaisharonmotsatsi@gmail.com>
2026-05-26 22:18:21 +0200

    Complete variable.js file

---
+++ variables.js
@@ -0,0 +1,36 @@
+// Exercise 1: Variable Declaration
+
+let name = "Sharon";
+let age = 24;
+let isStudent = false;
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Is Student:", isStudent);
+
+
+// Exercise 2: Variable Reassignment
+
+let favouriteColour = "Grey";
+
+console.log("Favourite Colour:", favouriteColour);
+
+favouriteColour = "White";
+
+console.log("New Favourite Colour:", favouriteColour);
+
+
+// Exercise 3: Arithmetic Operations
+
+let num1 = 20;
+let num2 = 5;
+
+let sum = num1 + num2;
+let difference = num1 - num2;
+let product = num1 * num2;
+let quotient = num1 / num2;
+
+console.log("Sum:", sum);
+console.log("Difference:", difference);
+console.log("Product:", product);
+console.log("Quotient:", quotient);
